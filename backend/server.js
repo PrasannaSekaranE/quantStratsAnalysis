8577786117a3348190900f6b33c8ae82a6d8f904
Prasannasekaran Elaangovan <prasannasekaran@xerago.com>
2026-01-08 16:07:08 +0530

    Trading 8 Jan_v2

--- backend/server.js
+++ backend/server.js
@@ -43,6 +43,8 @@ const CSV_FILES = [
   'confluence_trades_2025-12-30_153100.csv',
   'confluence_trades_2026-01-02_153100.csv',
   'live_trades_20260108_120558.csv',
+  'trades_20260108.csv',
+  'confluence_trades_2026-01-08_153103.csv',
   'trades_20260102.csv',
   'live_trades_20251231_152554.csv',
   'live_trades_20260102_115833.csv',
