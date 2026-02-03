16f8ba0d1987589fbb9e5eb589232903f8f49f1a
Prasannasekaran Elaangovan <prasannasekaran@xerago.com>
2026-02-03 08:43:22 +0530

    Feb 3_v1

--- backend/server.js
+++ backend/server.js
@@ -118,7 +118,8 @@ const CSV_FILES = [
   'V1_20260202_154323.csv',
   'V1_20260202_154313.csv',
   'trades_20260202.csv',
-  'confluence_trades_2026-02-02_153101.csv'
+  'confluence_trades_2026-02-02_153101.csv',
+  'BLAZE_20260202_153211.csv'
 ];
 
 const fs = require('fs');
